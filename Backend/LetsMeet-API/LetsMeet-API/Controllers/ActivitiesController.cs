9e8e6ca7171a9e1836236a82e57f5c7d8d842d84
NemesLaszlo <wow.laszlo@gmail.com>
2021-03-17 18:10:09 +0100

    BaseApiController for the future andpoints

---
+++ Backend/LetsMeet-API/LetsMeet-API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsMeet_API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetActivities()
+        {
+            return Ok(await _context.Activities.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetActivity(Guid id)
+        {
+            return Ok(await _context.Activities.FindAsync(id));
+        }
+    }
+}
