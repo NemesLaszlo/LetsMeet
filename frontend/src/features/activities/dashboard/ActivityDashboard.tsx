e705d035effef0aadedc0f12a9750a7cb02932b8
NemesLaszlo <wow.laszlo@gmail.com>
2021-03-25 14:23:16 +0100

    frontend router and links

--- frontend/src/features/activities/dashboard/ActivityDashboard.tsx
+++ frontend/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,21 @@
+import React, { useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store'
 import ActivityDetails from '../details/ActivityDetails'
 import ActivityForm from '../form/ActivityForm'
 import ActivityList from './ActivityList'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 const ActivityDashboard = () => {
     const {activityStore} = useStore()
     const {selectedActivity, editMode} = activityStore
+
+    useEffect(() => {
+      activityStore.loadActivities()
+    }, [activityStore])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='loading app' />
 
     return (
         <Grid>
