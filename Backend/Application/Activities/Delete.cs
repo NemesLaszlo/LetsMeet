397829d8dbda9db00bcb507ecd3d2790859a7f91
NemesLaszlo <wow.laszlo@gmail.com>
2021-03-27 00:06:10 +0100

    Exception handling with ExceptionMiddleware

--- Backend/Application/Activities/Delete.cs
+++ Backend/Application/Activities/Delete.cs
@@ -28,7 +28,6 @@ namespace Application.Activities
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
-                if (activity == null) return null;
 
                 _context.Remove(activity);
                 var result = await _context.SaveChangesAsync() > 0;
--- Backend/LetsMeet-API/LetsMeet-API/Startup.cs
+++ Backend/LetsMeet-API/LetsMeet-API/Startup.cs
@@ -1,6 +1,7 @@
 using Application.Activities;
 using FluentValidation.AspNetCore;
 using LetsMeet_API.Extensions;
+using LetsMeet_API.MIddleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -31,12 +32,15 @@ namespace LetsMeet_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LetsMeet_API v1"));
             }
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
