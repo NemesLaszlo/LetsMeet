97c9275f85c5f2b743718253cc37a7877b82423a
NemesLaszlo <wow.laszlo@gmail.com>
2021-03-29 16:50:54 +0200

    Infrastructure - Security - getUsername , migrations and Create modification + models to Many-to-Many

--- Backend/Domain/Activity.cs
+++ Backend/Domain/Activity.cs
@@ -16,5 +16,6 @@ namespace Domain
         public string City { get; set; }
         public string Venue { get; set; }
         public bool IsCancelled { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
     }
 }
--- Backend/LetsMeet-API/LetsMeet-API/Extensions/ApplicationServiceExtensions.cs
+++ Backend/LetsMeet-API/LetsMeet-API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Activities;
 using Application.Core;
+using Application.Interfaces;
 using Database;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -30,6 +32,7 @@ namespace LetsMeet_API.Extensions
 
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddScoped<IUserAccessor, UserAccessor>();
 
             services.AddSwaggerGen(c =>
             {
