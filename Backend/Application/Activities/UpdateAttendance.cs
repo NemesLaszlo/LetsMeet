7c5db3dfcac47687e6d5796d8875a8429d8568d0
NemesLaszlo <wow.laszlo@gmail.com>
2021-03-30 23:06:14 +0200

    UpdateAttendance endpoint to joint to the activity or leave, or the host cancel tha activity or re open it

---
+++ Backend/Application/Activities/UpdateAttendance.cs
@@ -0,0 +1,84 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Database;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class UpdateAttendance
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            // Id of the activity
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities
+                    .Include(a => a.Attendees).ThenInclude(u => u.AppUser)
+                    .SingleOrDefaultAsync(x => x.Id == request.Id);
+
+                if (activity == null) return null;
+
+                // Logged in user
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                if (user == null) return null;
+
+                // Name of the host of the activity
+                var hostUsername = activity.Attendees.FirstOrDefault(x => x.IsHost)?.AppUser?.UserName;
+
+                // Loggen in user alredy participate (maybe the host) on the activity (subscribed to the activity and his/her name already on the list so we can get that user) or not on the "list"
+                var attendance = activity.Attendees.FirstOrDefault(x => x.AppUser.UserName == user.UserName);
+
+                // Host can cancel the activity, but the host cant leave
+                if(attendance != null && hostUsername == user.UserName)
+                {
+                    activity.IsCancelled = !activity.IsCancelled;
+                }
+
+                // Non host users can leave
+                if (attendance != null && hostUsername != user.UserName)
+                {
+                    activity.Attendees.Remove(attendance);
+                }
+
+                // Join to the activity if you are (loggen in user) not on the list
+                if (attendance == null)
+                {
+                    attendance = new ActivityAttendee
+                    {
+                        AppUser = user,
+                        Activity = activity,
+                        IsHost = false
+                    };
+
+                    activity.Attendees.Add(attendance);
+                }
+
+                var result = await _context.SaveChangesAsync() > 0;
+                return result ? Result<Unit>.Success(Unit.Value) : Result<Unit>.Failure("Problem updating attendance");
+            }
+        }
+    }
+}
--- Backend/LetsMeet-API/LetsMeet-API/Controllers/ActivitiesController.cs
+++ Backend/LetsMeet-API/LetsMeet-API/Controllers/ActivitiesController.cs
@@ -40,5 +40,12 @@ namespace LetsMeet_API.Controllers
         {
             return HandleResult(await Mediator.Send(new Delete.Command {Id = id }));
         }
+
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
+        }
     }
 }
