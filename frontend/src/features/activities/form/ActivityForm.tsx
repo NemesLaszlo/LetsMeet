d72fc195bcf7cefdc470f74b19a24cf9329ce046
NemesLaszlo <wow.laszlo@gmail.com>
2021-03-31 22:55:59 +0200

    frontend update and create activity refactor because of attendees

--- frontend/src/features/activities/form/ActivityForm.tsx
+++ frontend/src/features/activities/form/ActivityForm.tsx
@@ -13,7 +13,7 @@ import MyTextArea from '../../../app/common/form/MyTextArea'
 import MySelectInput from '../../../app/common/form/MySelectInput'
 import { categoryOptions } from '../../../app/common/options/categoryOptions'
 import MyDateInput from '../../../app/common/form/MyDateInput'
-import { Activity } from '../../../app/models/activity'
+import { ActivityFormValues } from '../../../app/models/activity'
 
 const ActivityForm = () => {
     const history = useHistory()
@@ -21,15 +21,7 @@ const ActivityForm = () => {
     const {createActivity, updateActivity, loadActivity, loadingInitial} = activityStore
     const {id} = useParams<{id: string}>()
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    })
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -42,12 +34,12 @@ const ActivityForm = () => {
 
     useEffect(() => {
         if(id) {
-            loadActivity(id).then(activity => setActivity(activity!)) 
+            loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity))) 
         }
     }, [id, loadActivity])
     
-    const handleFormSubmit = (activity: Activity) => {
-        if(activity.id.length === 0) {
+    const handleFormSubmit = (activity: ActivityFormValues) => {
+        if(!activity.id) {
             let newActivity = {
                 ...activity,
                 id: uuid()
