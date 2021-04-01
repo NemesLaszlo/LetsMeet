4a425b9df6d6bec333667d1504a987f89651117e
NemesLaszlo <wow.laszlo@gmail.com>
2021-04-01 18:18:56 +0200

    Cloudinary api settiing and upload + delete functionality implementation

--- Backend/LetsMeet-API/LetsMeet-API/Extensions/ApplicationServiceExtensions.cs
+++ Backend/LetsMeet-API/LetsMeet-API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@
 using Application.Core;
 using Application.Interfaces;
 using Database;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +34,8 @@ namespace LetsMeet_API.Extensions
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             services.AddSwaggerGen(c =>
             {
